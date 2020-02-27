ed4b5eda0690f81534483bdaf56f44277303b2e9
BolanleD <wtlstudents@umn.edu>
2020-02-26 23:51:55 -0600

    Remove all comments in addSetUp branch

--- Project1.ino
+++ Project1.ino
@@ -1,11 +1,8 @@
 void setup() {
-  // put your setup code here, to run once:
-  // this is a new branch and changes have been made to it
+
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  // Made another change to the addSetUp branch
-
+ 
 }
